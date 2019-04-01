5f4ca81d1aca111b952a7fe381feedc9c2de26e6
Vishwal P K <vishwalpk3@gmail.com>
2019-04-01 16:49:30 +0530

    Added custom theme variable support

--- src/UIZeroThemeProvider/UIZeroThemeProvider.js
+++ src/UIZeroThemeProvider/UIZeroThemeProvider.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
-import getComputedStyleAttributeValue from '../utils/getComputedStyleAttributeValue';
+export default function ZeroUIThemeProvider({ children, theme = {} }) {
+	const defaultTheme = {
+		primaryColor: '#2F58F2',
+		secondaryColor: '#FF425C'
+	};
 
-export default function ZeroUIThemeProvider({ children, theme }) {
 	const computedTheme = {
-		primaryColor: getComputedStyleAttributeValue(
-			theme && theme.primaryColor,
-			'#2F58F2'
-		),
-		secondaryColor: getComputedStyleAttributeValue(
-			theme && theme.secondaryColor,
-			'#FF425C'
-		)
+		...defaultTheme,
+		...theme
 	};
 
 	return <ThemeProvider theme={computedTheme}>{children}</ThemeProvider>;
